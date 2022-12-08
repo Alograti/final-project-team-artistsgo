beaea958847640d965ff6e5974e17c6a349dfa94
Derek Han <82979043+Alograti@users.noreply.github.com>
2022-12-08 07:58:57 -0800

    Update Passport.js

--- back-end/Passport.js
+++ back-end/Passport.js
@@ -14,7 +14,7 @@ require('dotenv').config( { path: '../../.env' } );
 passport.use(new GoogleStrategy({
     clientID: "858364270611-n9baol12m5cjh86n47ruam7uci496co4.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "http://localhost:3001/auth/google/callback",
+    callbackURL: "http://192.241.146.229:3001/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
